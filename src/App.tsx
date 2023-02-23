5c892e52d6d1a668d73b47c8268ee58949789e2b
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-23 16:30:39 -0300

    feat: test routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import '@/css/App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
-    </div>
+    <Routes>
+      <Route path="/">
+        <h1>home</h1>
+      </Route>
+      <Route path="/login">
+        <h1>login</h1>
+      </Route>
+      <Route path="*">
+        <h1>404</h1>
+      </Route>
+    </Routes>
   )
 }
 
