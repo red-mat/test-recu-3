9f66e51ded315a275f88b28b7c8ee6067ad3f17b
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-23 19:45:05 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import '@/css/App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
+
+function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <Link to="/login">login</Link>
+    </main>
+  )
+}
 
 function App() {
-  return <h1>Home</h1>
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<h1>login</h1>} />
+      <Route path="*" element={<h1>404</h1>} />
+    </Routes>
+  )
 }
 
 export default App
