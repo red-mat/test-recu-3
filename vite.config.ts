f2d9cbd523bd9a6bab8f3cfec5111a50d8f15c34
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-22 22:12:10 -0300

    feat: setting enviroment testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path';
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/test-recu-3/',
+  test: { environment: 'happy-dom' },
   resolve: {
     alias: {
       '@': path.resolve('src'),
